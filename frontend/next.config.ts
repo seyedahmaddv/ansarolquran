53e870d8a9df3edda628f63d8ca3e77678c0db50
Seyed Ahmad <seyedahmaddv@gmail.com>
2025-04-15 22:10:51 +0330

    add static export for Netlify

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,6 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  };
-
-export default nextConfig;
+export default nextConfig
