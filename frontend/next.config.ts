0bec036aa3be418cde5a7b732ed859b7c6564bd5
Seyed Ahmad <seyedahmaddv@gmail.com>
2025-04-15 22:59:50 +0330

    modify Header component

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  images: {
+    unoptimized: true, // ✅ اجازه می‌ده تصاویر بدون بهینه‌سازی لود بشن
+  },
 }
 
 export default nextConfig
