85ec08f85ca246472964bd71805a698638a50c02
Seyed Ahmad <seyedahmaddv@gmail.com>
2025-04-16 14:53:45 +0330

    modify header component: className='absolute top-full -left-8 ...' to className='absolute top-full right-8 ...'

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -71,7 +71,7 @@ export default function Example() {
 
             <PopoverPanel
               transition
-              className="absolute top-full -left-8 z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5 transition data-closed:translate-y-1 data-closed:opacity-0 data-enter:duration-200 data-enter:ease-out data-leave:duration-150 data-leave:ease-in"
+              className="absolute top-full right-8 z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5 transition data-closed:translate-y-1 data-closed:opacity-0 data-enter:duration-200 data-enter:ease-out data-leave:duration-150 data-leave:ease-in"
             >
               <div className="p-4">
                 {products.map((item) => (
